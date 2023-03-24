5002f6abcb5c33190ef14a9806e60cd7ddc6ad4c
Andrew Robles <andrewrobles@verizon.net>
2023-03-24 10:54:57 -0700

    wip

--- code/todo/models.py
+++ code/todo/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class TodoItem(models.Model):
     text = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
+
+    @property
+    def striked_text(self):
+        return ''.join([letter + '\u0336' for letter in self.text])
     
     def __str__(self):
-        return self.text
+        return self.text if not self.done else self.striked_text
